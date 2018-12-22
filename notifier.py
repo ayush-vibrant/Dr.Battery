34f9981c2000c50d28dc7562e2bb2344b663c5b1
ayush-vibrant <ayushvibrant@gmail.com>
2018-12-23 00:19:21 +0530

    added desktop notifier

---
+++ notifier.py
@@ -0,0 +1,25 @@
+import notify2 
+import battery as b
+  
+# path to notification icon 
+ICON_PATH = '/home/ayush/Desktop/Dr.Battery/battery2.jpg'
+
+notify2.uninit()
+
+# initialise the d-bus connection 
+notify2.init("Dr.Battery!!") 
+  
+# create Notification object 
+n = notify2.Notification(None, icon = ICON_PATH) 
+
+# set urgency level 
+n.set_urgency(notify2.URGENCY_NORMAL) 
+  
+# set timeout for a notification 
+n.set_timeout(1000) 
+
+n.update(b.message)
+n.show()
+n.close()
+
+ 
